4ffb1caa9a233ec42a3ba2d9e45a5272553e89c2
EstradaMezaReneAlejandro <rene.estrada17@tectijuana.edu.mx>
2023-12-27 14:25:15 -0800

    second commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -8,10 +8,15 @@ import Login from "./pages/Login";
 import PageNav from "./components/PageNav";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App()
 {
-    const [cities, setCities] = useState({});
+    const [cities, setCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(
@@ -19,8 +24,23 @@ export default function App()
         {
             async function fetchCities()
             {
-                
+                try 
+                {
+                    setIsLoading(true);
+                    const res = await fetch(`${BASE_URL}/cities`);
+                    const data = await res.json();
+                    setCities(data);
+                }
+                catch 
+                {
+                    alert("There was an error");
+                }
+                finally
+                {
+                    setIsLoading(false);
+                }
             }
+            fetchCities()
         },
         []
     );
@@ -34,10 +54,32 @@ export default function App()
                     <Route path="pricing" element={<Pricing />} />
                     <Route path="login" element={<Login />} />
                     <Route path="applayout" element={<AppLayout />}>
-                        <Route index element={<CityList />} />
-                        <Route path="cities" element={<CityList />}/>
-                        <Route path="countries" element={<p>list of countries</p>}/>
-                        <Route path="form" element={<p>form</p>}/>
+                        <Route 
+                            index element={<Navigate replace to="cities"/>}
+                        />
+                        <Route 
+                            path="cities" 
+                            element={
+                                <CityList 
+                                    cities={cities} 
+                                    isLoading={isLoading}
+                                />
+                            }
+                        />
+                        <Route
+                            path="cities/:id"
+                            element={<City />} 
+                        />
+                        <Route 
+                            path="countries" 
+                            element={
+                                <CountryList 
+                                    cities={cities} 
+                                    isLoading={isLoading} 
+                                />
+                            }
+                        />
+                        <Route path="form" element={<Form />}/>
                     </Route>
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,9 +1,20 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+
+    const navigate = useNavigate();
+
+    const [serchParams, setSearchParams] = useSearchParams();
+
+    const lat = serchParams.get("lat");
+    const lng = serchParams.get("lng");
+
     return (
-        <div className={styles.mapContainer}>
-            
+        <div className={styles.mapContainer} onClick={() => {navigate("form")}}>
+            <h1>Map</h1>
+            <h1>position: {lat}, {lng}</h1>
+            <button onClick={() => {setSearchParams({lat: 23, lng: 50})}}>Change position</button>
         </div>
     )
 }
